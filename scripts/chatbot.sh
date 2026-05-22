d995e44e98a187ccbbc17338bdf855ebb0b5873f
ninakokalj <162151394+ninakokalj@users.noreply.github.com>
2026-05-22 20:47:44 +0200

    Update chatbot.sh

--- scripts/chatbot.sh
+++ scripts/chatbot.sh
@@ -7,10 +7,14 @@
 #SBATCH --mem-per-gpu=64G
 #SBATCH --time=00:30:00
 #SBATCH --output=logs/chat-%J.out
-#SBATCH --error=logs/error-%J.err
+#SBATCH --error=errors/error-%J.err
 #SBATCH --job-name="travel_chatbot"
 
 module load Python/3.10.8-GCCcore-12.2.0
+
+mkdir -p logs
+mkdir -p errors
+
 source my_env/bin/activate
 
 srun python code/chatbot.py "$1"
